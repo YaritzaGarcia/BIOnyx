68b8b0112aa9eab4ac81f2334223f547d5755cd8
yaritza.garcia3 <yaritza.garcia3@upr.edu>
2021-11-18 14:19:08 -0400

    Testing some libraries

---
+++ testingBioPython.py
@@ -0,0 +1,26 @@
+from Bio.PDB import *
+import nglview as nv
+
+# https://www.tutorialspoint.com/biopython/biopython_pdb_module.htm
+
+pdbl = PDBList() 
+
+# Getting pdb file
+pdbl.retrieve_pdb_file('2FAT', pdir = '.', file_format = 'pdb')
+pdb_parser = PDBParser(QUIET = True)
+# Modeling pdb file 
+data = pdb_parser.get_structure("2FAT", "2FAT.pdb")
+view = nv.show_biopython(data)
+view
+
+# Getting mmCif file
+pdbl.retrieve_pdb_file('2FAT', pdir = '.', file_format = 'mmCif')
+# Modeling cif file
+parser = MMCIFParser(QUIET = True) 
+data = parser.get_structure("2FAT", "2FAT.cif")
+view = nv.show_biopython(data)
+view
+
+
+
+     
