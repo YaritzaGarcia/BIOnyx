34be5eea2fff08cee57558e2573c9a5be92f49fa
yaritza.garcia3 <yaritza.garcia3@upr.edu>
2021-11-20 17:05:03 -0400

    new method! getPolypeptides(ID)

--- utilities.py
+++ utilities.py
@@ -1,38 +1,37 @@
 from Bio.PDB import *
+from Bio.PDB.MMCIF2Dict import *
 import nglview as nv
-import ipywidgets as widget
+from texttable import Texttable
+import sys, os
 
+# Disable
+def blockPrint():
+    sys.stdout = open(os.devnull, 'w')
+
+# Restore
+def enablePrint():
+    sys.stdout = sys.__stdout__
+
+# Print bold
+start = "\033[1m"
+end = "\033[0;0m"
 
 # # https://www.tutorialspoint.com/biopython/biopython_pdb_module.htm
-
-# pdbl = PDBList()
-
-# # Getting pdb file
-# pdbl.retrieve_pdb_file('2FAT', pdir = '.', file_format = 'pdb')
-# pdb_parser = PDBParser(QUIET = True)
-# # Modeling pdb file
-# data = pdb_parser.get_structure("2FAT", "2FAT.pdb")
-# view = nv.show_biopython(data)
-# view
-
-# # Getting mmCif file
-# pdbl.retrieve_pdb_file('2FAT', pdir='.', file_format='mmCif')
-# # Modeling cif file
-# parser = MMCIFParser(QUIET=True)
-# data = parser.get_structure("2FAT", "2FAT.cif")
-# view = nv.show_biopython(data)
-# view
-
 
 def infoID(ID):
     idPdb = PDBList()
     idPdb.retrieve_pdb_file(ID, pdir='.', file_format='mmCif')
-    mmcif_Parser = MMCIFParser(QUIET=True)
     fileName = ID + ".cif"
-    structure = mmcif_Parser.get_structure(ID, fileName.lower())
-    return len(structure.get_residues())
+    # mmcif_dict = MMCIF2Dict(fileName)
+    mmcif_Parser = MMCIFParser(QUIET=True)
+    idStructure = mmcif_Parser.get_structure(ID, fileName.lower())
+    # for idModel in idStructure:
+    #     for residue in idModel.get_residues():
+    #         print(residue)
+    residues = idStructure.get_residues() # returns a generator object
+    [print(item) for item in residues]
 
-# print(infoID("1atp"))
+# infoID("1atp")
 
 def model(ID):
     # Searching for the pdb file
@@ -43,15 +42,68 @@ def model(ID):
     mmcif_Parser = MMCIFParser(QUIET=True)
     fileName = ID + ".cif"
     idStructure = mmcif_Parser.get_structure(ID, fileName.lower())
+    
+    for model in idStructure:
+        for chain in model:
+            print('%s - Chain: %s. Number of residues: %d. Number of atoms: %d.')
 
-    # Modeling the protein structure
-    idModel = nv.show_biopython(idStructure)
-    return(idModel)
+    # # Modeling the protein structure
+    # idModel = nv.show_biopython(idStructure)
+    # return(idModel)
 
-model("2FAT")
+# model("2FAT")
 
 def getFile(ID, path):
     # Searching for the pdb file
     pdbServer = PDBList()
     pdbServer.retrieve_pdb_file(ID, pdir=path, file_format='mmCif')
-    print("The file has been saved in " + path)
+    return("The file has been saved in " + path)
+
+def getPolypeptides(ID):
+
+    # Creating rows for the Polypeptide table
+    polypeptidesRows = [
+        ["Polypeptide Number", "Polypeptide Sequence", "Sequence Length"]]
+
+    # Searching for the pdb file
+    blockPrint()
+    getFile(ID, '.')
+    enablePrint()
+
+    # Getting the protein structure
+    mmcif_Parser = MMCIFParser(QUIET=True)
+    fileName = ID + ".cif"
+    idStructure = mmcif_Parser.get_structure(ID, fileName.lower())
+
+    # Getting Polypeptide information
+    builder = PPBuilder()
+    polypeptideNum = 1
+    for polypeptide in builder.build_peptides(idStructure):
+        seq = polypeptide.get_sequence()
+        polypeptidesRows.append([polypeptideNum, seq, len(seq)])
+        polypeptideNum += 1
+
+    # Creating and printing the table
+    polypeptidesTable = Texttable()
+    polypeptidesTable.add_rows(polypeptidesRows)
+    polypeptidesTable.set_cols_align(['c', 'c', 'c'])
+    print("This table contains all the polypeptides of the protein " + start + ID + end)
+    print(polypeptidesTable.draw())
+
+# getPolypeptides("2FAT")
+
+
+def getAminoAcids(ID):
+    aminoAcidsTable = Texttable()
+    aminoAcidsTable.add_rows(
+        [
+            ["No.", "Name", "Grade", "Age"],
+            [1, "Bob", 6, 11],
+            [2, "Freddy", 4, 10],
+            [3, "John", 7, 13],
+        ]
+    )
+    aminoAcidsTable.set_cols_align(['c', 'c', 'c', 'c'])
+    print(aminoAcidsTable.draw())
+
+# getAminoAcids("1atp")
